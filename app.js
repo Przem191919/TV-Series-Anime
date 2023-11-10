9d39babe0ae3f728b470e199df62f3b91e2a4f5e
przem <przem.olejnik@gmail.com>
2023-11-10 09:36:39 +0100

    dodanie hedera i swipera

---
+++ app.js
@@ -0,0 +1,53 @@
+var titles = {
+    0: {
+        title: "",
+        subTitle: "",
+        desc: "",
+    },
+    1: {
+        title: "",
+        subTitle: "",
+        desc: "",
+    },
+    2: {
+        title: "",
+        subTitle: "",
+        desc: "",
+    },
+    3: {
+        title: "",
+        subTitle: "",
+        desc: "",
+    },
+    4: {
+        title: "",
+        subTitle: "",
+        desc: "",
+    },
+    5: {
+        title: "",
+        subTitle: "",
+        desc: "",
+    },
+}
+
+//Swiper
+
+const swiper = new Swiper('.bannerSwiper', {
+
+    spaceBetween: 0,
+    effect: 'fade',
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clicable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
